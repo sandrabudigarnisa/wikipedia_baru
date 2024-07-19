ad419ce33f0ee077d61d47eb48597afe3066f8f9
“sandrabudigarnisa“ <sandrabudigarnisa.09@gmail.com>
2024-07-19 15:04:17 +0700

    new

---
+++ Test Suites/search DDT.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>search DDT</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>08ac86be-d81a-4166-bd32-560c67d2aea4</testSuiteGuid>
+   <testCaseLink>
+      <guid>80b712c5-66f8-4d11-a571-395b50f0e767</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search DDT</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>53657b7e-ecf7-4c9d-8f0d-8a55182ccbc8</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data Test</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>53657b7e-ecf7-4c9d-8f0d-8a55182ccbc8</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Teks</value>
+         <variableId>9b4fd3b5-c499-46cb-bf69-a919ac0ff3e6</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
